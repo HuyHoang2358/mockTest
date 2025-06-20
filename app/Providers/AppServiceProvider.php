290abe0da80e9821ca2257ce55fc4ea665d1b924
huyhoang2358 <huyhoang.tran6669@gmail.com>
2025-06-20 13:47:59 +0700

    fix session

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Session\DatabaseSessionHandler;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +24,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
     }
 }
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,11 +15,20 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         for ($i = 1; $i <= 20; $i++) {
-            Admin::create([
+            $admin = Admin::create([
                 'name' => 'Admin ' . $i,
                 'email' => 'admin' . $i . '@example.com',
                 'password' => Hash::make('password123'), // Hoặc bcrypt('password123')
             ]);
+            $admin->profile()->updateOrCreate(
+                ['admin_id' => $admin->id],
+                [
+                    'avatar' => 'https://ui-avatars.com/api/?name=Admin+' . $i . '&background=random&color=fff',
+                    'phone' => '0123456789',
+                    'address' => 'Hà Nội',
+                    'birthday' => '2000-01-01',
+                ]
+            );
         }
     }
 }
